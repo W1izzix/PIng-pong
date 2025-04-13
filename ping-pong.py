3276d33e77a536c437cab6d1c360ea73a8d3766f
W1izzix <93875403+W1izzix@users.noreply.github.com>
2025-04-13 13:25:07 +0500

    Add files via upload

--- ping-pong.py
+++ ping-pong.py
@@ -1,9 +1,11 @@
 from pygame import *
+from time import time as timer
 FPS = 60
 clock = time.Clock()
 game = True
 finish = False
-speed = 10
+speed_x = 5
+speed_y = 5
 window = display.set_mode((700,500))
 class GameSprite(sprite.Sprite):
     def __init__(self,player_image,player_x,player_y,player_speed,size_x,size_y):
@@ -29,19 +31,13 @@ class Player2(GameSprite):
             self.rect.y -= self.speed
         if keys_pressed[K_DOWN]and self.rect.y < 370:
             self.rect.y += self.speed
-# class Myach(GameSprite):
-#     def update(self):
-#         keys_pressed = key.get_pressed()
-#         if keys_pressed[K_y] and self.rect.y > 5:
-#             self.rect.y -= self.speed
-#         if keys_pressed[K_h]and self.rect.y < 370:
-#             self.rect.y += self.speed
-class Myach(GameSprite):
-    def update():
-        pass
+font.init()
+font1 = font.SysFont('Arial',36)
+levo = font1.render('Player1 win',10,(255,0,0))
+pravo = font1.render('Player2 win',10,(0,255,0))
 maga = Player('raketka.png',5,250,10,80,150)
 maga2 = Player2('raketka.png',620,250,10,80,150)
-ball = Myach('myach.png',350,150,10,50,50)
+ball = GameSprite('myach.png',340,150,10,50,50)
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -54,13 +50,37 @@ while game:
         maga2.reset()
         maga2.update()
         ball.reset()
-        ball.rect.x += speed
-        ball.rect.y += speed
-        if ball.rect.y < 0:
-            speed *= -1
-        if ball.rect.x > 450 or ball.rect.x < 0:
-            speed *= -1 
-        if ball.spritecollide(maga,maga2):
-            speed *= -1
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+    if sprite.collide_rect(maga,ball) or sprite.collide_rect(maga2,ball):
+        speed_x *= -1
+    if ball.rect.y < 0 or ball.rect.y > 470:
+        speed_y *= -1
+    # if ball.rect.x > 700 or ball.rect.x < 0:
+    #     speed_x *= -1 
+    if ball.rect.x < -10:
+        window.blit(pravo,(200,200))
+        # ball.kill()
+        # maga.kill()
+        # maga2.kill()
+        # window.blit(pravo,(200,200))
+        # time.delay(5000)
+        # maga.reset()
+        # maga.update()
+        # maga2.reset()
+        # maga2.update()
+        # ball.reset()
+    if ball.rect.x > 710:
+        window.blit(levo,(200,200))
+        # ball.kill()
+        # maga.kill()
+        # maga2.kill()
+        # window.blit(levo,(200,200))
+        # time.delay(5000)
+        # maga.reset()
+        # maga.update()
+        # maga2.reset()
+        # maga2.update()
+        # ball.reset()
     clock.tick(FPS)
     display.update()
